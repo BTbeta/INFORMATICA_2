15e8985e117ae97d4311289823356d3daa00379c
BTbeta <113897620+BTbeta@users.noreply.github.com>
2022-11-06 16:07:44 +0100

    Update file1.cpp

--- 4H/file1.cpp
+++ 4H/file1.cpp
@@ -2,19 +2,17 @@
 
 int main()
 {
-	char dio[]={"C:\\Users\\Gejin\\OneDrive\\Desktop\\programmi_di_scuola\\2022_2023"};
-	int err;
-	char c;
-	FILE *puntaFile;
-	puntaFile=fopen_s(dio,"r");
+	int c;
+	FILE* puntaFile;
+	fopen_s(&puntaFile,"dio.txt","r");
 	if(puntaFile==0)
 	{
-		while(!feof(puntaFile))
+		while(feof(puntaFile))
 		{
 			c=fgetc(puntaFile);
 			printf("%s",c);
 		}
-		err=fclose(puntaFile);
+		fclose(puntaFile);
 	}
 	else
 	{
