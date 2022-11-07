84b4bc588b02567ff5771ab0e4b55bdb0b895c62
BTbeta <113897620+BTbeta@users.noreply.github.com>
2022-11-07 11:22:08 +0100

    Update file2.cpp

--- 4H/file2.cpp
+++ 4H/file2.cpp
@@ -1,24 +1,26 @@
 #include<stdio.h>
+#define MAXSTR 20
+#define PAROLE 3
 
 int main()
 {
 	char buffer[MAXSTR+1];
 	char nomeFileIN[]={"dio.txt"};
-	printf("nome del file da scrivere:");
-	scanf("%c"nomeFileIN);
-	int err1;
+	//printf("nome del file da scrivere:");
+	//scanf("%s",nomeFileIN);
+	int err;
 	FILE* pFile;
-	fopen_s(&pFile,nomeFileIN,"w");
-	if(err1==0)
+	pFile=fopen(nomeFileIN,"w");
+	if(pFile!=NULL)
 	{
-		for(int i=0;i<PAROLE;x++)
+		for(int i=0;i<PAROLE;i++)
 		{
 			printf("inserisci il nome del frutto:");
-			scanf("%s"buffer);
+			scanf("%s",buffer);
 			fputs(buffer,pFile);
 			fputc((int)'\n',pFile);
 		}
-		err=fclose(puntaFile);
+		err=fclose(pFile);
 	}
 	else
 	{
