d144b2f61270b0cc101e18672742a941bd1f3c10
BTbeta <113897620+BTbeta@users.noreply.github.com>
2022-11-23 16:14:45 +0100

    Update DES01_PAG30_E.c

--- 4H/D-file/DES01_PAG30_E.c
+++ 4H/D-file/DES01_PAG30_E.c
@@ -9,6 +9,7 @@
 * @date <12/11/2022> 
 * @version 1.0 <14/11/2022> Versione iniziale
 * @version 1.1 <16/11/2022> <completamento del programma e comenti>
+* @version 1.2 <23/11/2022> <si crea un secondo file ,visualizza l'apertura del file e modifiche nelle funzioni>
 */
 #include<stdio.h>
 
@@ -19,9 +20,9 @@
 * @see <rand serve genera numeri casuali e con srand nel main serve per funzionare rand sono nella libreia stdlib.h e time.h>
 *
 * @author <zeng oscar>
-* @version 1.0 <data> <Descrivere le modifiche apportate>
+* @version 1.0 <23/11/2022> <nuovo algoritmo>
 */
-void cesareCrypt(char[]);
+void cesareCrypt(char[],char[]);
 
 /** ****************************************************************************************
 * @brief <decrypta il messagio nel file di testo>
@@ -30,65 +31,75 @@ void cesareCrypt(char[]);
 * @see <rand serve genera numeri casuali e con srand nel main serve per funzionare rand sono nella libreia stdlib.h e time.h>
 *
 * @author <zeng oscar>
-* @version 1.0 <data> <Descrivere le modifiche apportate>
+* @version 1.0 <23/11/2022> <nuovo algoritmo>
 */
-void cesareDecrypt(char[]);
+void cesareDecrypt(char[],char[]);
 
 int main()
 {
-	char dio[]={"dio.txt"};							//dichiarando il nomo del file
-	FILE* pFile1;									//il segno del file dio
-	pFile1=fopen(dio,"r");							//apre numeri
-	cesareCrypt(dio);								//cripta il messagio
-	cesareDecrypt(dio);								//de cripta il messagio
+	char file1[]={"dio.txt"},file2[]={"dio(1).txt"};//dichiarando i nomi dei file
+	int err1,err2;									//interi utilizzato per funzionare fclose
+	FILE* pFile1;									//il seglo del file1
+	FILE* pFile2;									//il segno del file2
+	char c;											//è utilizato per prendere le lettera e modificarli
+	pFile1=fopen(file1,"r");						//apre file1
+	pFile2=fopen(file2,"r+");						//apre file2
+	if(pFile1!=NULL&&pFile2!=NULL)					//se i due file si aprono
+	{
+		cesareCrypt(file1,file2);					//cripta il messagio
+		cesareDecrypt(file1,file2);					//de cripta il messagio
+		printf("operazione completo");
+	}
+	else											//se è falso visializza un errore
+	{
+		printf("\nil file non puo'essere aperto");
+	}
 }
 
-void cesareCrypt(char d[])
+void cesareCrypt(char f1[],char f2[])
 {
-	char c;											//è utilizato per prendere le lettera
-	int err;										//intero utilizzato per funzionare fclose
-	FILE* pFile;									//il segno del file dio
-	pFile=fopen(d,"w");								//apre dio
-	while(!feof(pFile))								//il ciclo continua fino quando è uguale a \0
+	char c;										//è utilizato per prendere le lettera
+	int err1,err2;								//interi utilizzato per funzionare fclose
+	FILE* pFile1;								//il seglo del file1
+	FILE* pFile2;								//il segno del file2
+	char c;										//è utilizato per prendere le lettera e modificarli
+	pFile1=fopen(f1,"r");						//apre file1
+	pFile2=fopen(f2,"w");						//apre file2
+	while(!feof(pFile1))						//il ciclo continua fino quando è uguale a \0
 	{
-		c=fgetc(pFile);								//prende un caratere
-		if(c>='a'&&c<='z'||c>='A'&&c<='Z')			//se è una letera
+		c=fgetc(pFile1);						//prende un caratere
+		c-=3									//mette 3 letere prima
+		if(c<'a'&&c>'Z'||c<'A')					//se è più picolo di A nel codice ascii
 		{
-			if(c!='a'||c!='A')						//se è a
-			{
-				if(c=='b'||c=='B')					//se è b
-				c+=24;								//mette z
-				else
-				c-=3;								//mette 3 letere prima
-			}
-			else
-			c+=24;									//mette y
+			c+=24;								//prende la letera correta
 		}
+		if(!feof(pFile1))						//se è diverso da \0
+		fputc(c,pFile2);						//se è vero lo mette nel file2
 	}
-	err=fclose(pFile);								//chiude file di testo
+	err1=fclose(pFile1);						//chiusura del file1
+	err2=fclose(pFile2);						//chiusura del file2
 }
 
-void cesareDecrypt(char d[])
+void cesareDecrypt(char f1[],char f2[])
 {
-	char c;											//è utilizato per prendere le lettera
-	int err;										//intero utilizzato per funzionare fclose
-	FILE* pFile;									//il segno del file dio
-	pFile=fopen(d,"w");								//apre dio
-	while(!feof(pFile))								//il ciclo continua fino quando è uguale a \0
+	char c;										//è utilizato per prendere le lettera
+	int err1,err2;								//interi utilizzato per funzionare fclose
+	FILE* pFile1;								//il seglo del file1
+	FILE* pFile2;								//il segno del file2
+	char c;										//è utilizato per prendere le lettera e modificarli
+	pFile1=fopen(f1,"r");						//apre file1
+	pFile2=fopen(f2,"w");						//apre file2
+	while(!feof(pFile1))						//il ciclo continua fino quando è uguale a \0
 	{
-		c=fgetc(pFile);								//prende un caratere
-		if(c>='a'&&c<='z'||c>='A'&&c<='Z')			//se è una letera
+		c=fgetc(pFile1);						//prende un caratere
+		c+=3									//mette 3 letere prima
+		if(c<'A'&&c>'z'||c>'Z')					//se è più grande di Z nel codice ascii
 		{
-			if(c!='y'||c!='Y')						//se è y
-			{
-				if(c=='z'||c=='Z')					//se è z
-				c-=24;								//mette b
-				else
-				c+=3;								//mette 3 letere dopo
-			}
-			else
-			c-=24;									//mette a
+			c-=24;								//prende la letera correta							
 		}
+		if(!feof(pFile1))						//se è diverso da \0
+		fputc(c,pFile2);						//se è vero lo mette nel file2
 	}
-	err=fclose(pFile);								//chiude file di testo
+	err1=fclose(pFile1);						//chiusura del file1
+	err2=fclose(pFile2);						//chiusura del file2
 }
