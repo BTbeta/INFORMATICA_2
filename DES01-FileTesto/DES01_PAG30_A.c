9b4758b10d4c97c21efe99e87d862cde77a75d46
BTbeta <113897620+BTbeta@users.noreply.github.com>
2022-12-10 16:38:41 +0100

    Update DES01_PAG30_A.c

--- DES01-FileTesto/DES01_PAG30_A.c
+++ DES01-FileTesto/DES01_PAG30_A.c
@@ -2,7 +2,7 @@
 * \mainpage <DES01_PAG30_A>
 *
 * @brief <inserire una breve descrizione del progetto>
-* <il programma copia le frasi dal file di testo del file1 e rende tutto maiuscolo e lo mette nel file di testo del file2>
+* <il programma legge e copia le frasi dal file di testo del file1 e rende tutto maiuscolo e lo mette nel file di testo del file2>
 * <se non si aprono i uno dei due file fa visualizzare un messagio di errore e se si apre visualozza il completamento>
 * 
 * @author <zeng osacr hao>
@@ -11,6 +11,7 @@
 * @version 1.1 <13/11/2022> <correzione del programma>
 * @version 1.2 <14/11/2022> <correzione del programma e comento>
 * @version 1.3 <23/11/2022> <si crea un secondo file>
+* @version 1.4 <10/12/2022> <aggiungere il visualizzazione>
 */
 #include<stdio.h>
 
@@ -28,6 +29,7 @@ int main()
 		while(!feof(pFile1))							//il ciclo continua fino quando è uguale a \0
 		{
 			c=fgetc(pFile1);							//prende una lettera 
+			printf("%c",c);								//vissualizza il file
 			if(c>='a'&&c<='z')							//se la lettera presa è minuscolo
 			c-=32;										//se è vero fa diventare la letera minuscola in maiuscola
 			if(!feof(pFile1))							//se è diverso da \0
@@ -35,7 +37,7 @@ int main()
 		}
 		err1=fclose(pFile1);							//chiusura del file1
 		err2=fclose(pFile2);							//chiusura del file2
-		printf("operazione completo");
+		printf("\noperazione completo");
 	}
 	else												//se è falso visializza un errore
 	{
