ebc6f5ce3fd731ce3adc8207463f30f2d952007d
BTbeta <113897620+BTbeta@users.noreply.github.com>
2023-10-28 20:13:12 +0200

    Update verifica.php

--- PHP|HTML/BES03/verifica.php
+++ PHP|HTML/BES03/verifica.php
@@ -18,69 +18,89 @@
 	$Nickname=$_POST["Nickname"];
 	$Password=$_POST["Password"];
 	
-	if (preg_match("/^[a-zA-Z ]$/", $Nome)) {
-	echo "Nome:"trim($Nome);
+	if (preg_match("/^[a-zA-Z ]*$/", $Nome)) {
+		echo "Nome:";
+		echo trim($Nome);
+		echo "<br>";
 	} else {
-	  echo "$Nome non si può mettere numeri e carateri speciali";
+	  echo "<font color='red'>$Nome non si può mettere numeri e carateri speciali</font>";
+	  echo "<br>";
 	}
-	
-	if (preg_match("/^[a-zA-Z' ]$/", $Cognome)) {
-	echo "Cognome:"trim($Cognome);
+
+	if (preg_match("/^[a-zA-Z' ]*$/", $Cognome)) {
+		echo "Cognome:";
+		echo trim($Cognome);
+		echo "<br>";
 	} else {
-	  echo "$Cognome non si può mettere numeri e carateri speciali ecceto'";
+	  echo "<font color='red'>$Cognome non si può mettere numeri e carateri speciali ecceto</font>";
+	  echo "<br>";
 	}
-	if (preg_match("/^[a-zA-Z0-9]$/", $Codice_fiscale)) {
-	echo "Codice fiscale:$Codice_fiscale";
+
+	if (preg_match("/^[a-zA-Z0-9]*$/", $Codice_fiscale)) {
+		echo "Codice fiscale:$Codice_fiscale<br>";
 	} else {
-	  echo "$Codice_fiscale non è valido";
+	  echo "<font color='red'>$Codice_fiscale non è valido</font>";
+	  echo "<br>";
 	}
 	
 	if (filter_var($Email, FILTER_VALIDATE_EMAIL)) {
-	echo "Email:$Email";
+		echo "Email:$Email";
+		echo trim($Email);
+		echo "<br>";
 	} else {
-	  echo "$email non è valido";
+	  echo "<font color='red'>$email non è valido</font>";
+	  echo "<br>";
 	}
 
-	if (preg_match("/^39[0-9]$/", $Cellulare)) {
-	echo "Cellulare:$Cellulare";
+	if (preg_match("/^39[0-9]*$/", $Cellulare)) {
+		echo "Cellulare:$Cellulare<br>";
 	} else {
-	  echo "$Cellulare non è valido";
+	  echo "<font color='red'>$Cellulare non è valido</font>";
 	}
 
-	if (preg_match("/^[a-zA-Z' ]$/", $via_piazza)) {
-	echo "Via/Piazza:"trim($via_piazza);
+	if (preg_match("/^[a-zA-Z' ]*$/", $via_piazza)) {
+		echo "Via/Piazza:";
+		echo trim($via_piazza);
+		echo "<br>";
 	} else {
-	  echo "$via_piazza non si può mettere carateri speciali e numeri";
+	  echo "<font color='red'>$via_piazza non si può mettere carateri speciali e numeri</font>";
+	  echo "<br>";
 	}
 	
-	if (preg_match("/^[0-9]{5}$/", $Cap)&&) {
-	echo "Cap:$Cap";
+	if (preg_match("/^[0-9]{5}$/", $Cap)) {
+		echo "Cap:$Cap<br>";
 	} else {
-	  echo "$Cap è sbagliato";
+	  echo "<font color='red'>$Cap è sbagliato</font>";
+	  echo "<br>";
 	}
 	
-	if (preg_match("/^[a-zA-Z' ]$/", $Comune)&&) {
-	echo "Comune:"trim($Comune);
+	if (preg_match("/^[a-zA-Z' ]*$/", $Comune)) {
+		echo "Comune:";
+		echo trim($Comune);
+		echo "<br>";
 	} else {
-	  echo "$Comune è sbagliato";
+	  echo "<font color='red'>$Comune è sbagliato</font>";
+	  echo "<br>";
 	}
 	
-	if (preg_match("/^[a-zA-Z' ]$/", $Provincia)&&) {
-	echo "Provincia:"trim($Provincia);
+	if (preg_match("/^[a-zA-Z' ]*$/", $Provincia)) {
+		echo "Provincia:";
+		echo trim($Provincia);
+		echo "<br>";
 	} else {
-	  echo "$Provincia è sbagliato";
+	  echo "<font color='red'>$Provincia è sbagliato</font>";
 	}
 	
-	if (preg_match("/^[a-zA-Z0-9_]$/", $Nickname)&&$Nickname!=$Nome) {
-	echo "Nickname:$Nickname";
+	if ($Nickname!=$Nome) {
+	echo "Nickname:$Nickname<br>";
 	} else {
-	  echo "$Nickname non si può mettere carateri speciali e numeri";
+	  echo "<font color='red'>$Nickname non si può mettere carateri speciali e numeri</font>";
 	}
 	
-	if (preg_match("/^[a-z]+[A-Z]+[0-9]+[ -/:-@[-'{-}]+$/", $Password)) {
+	if (preg_match("/^[[a-z]+[A-Z]+[0-9]+]{8,}+$/", $Password)) {
 	echo "Password:$Password";
 	} else {
-	  echo "$Password è troppo corto o non ha ragiunto i requesiti";
+	  echo "<font color='red'>$Password è troppo corto o non ha ragiunto i requesiti</font>";
 	}
 	?>
 </body>
