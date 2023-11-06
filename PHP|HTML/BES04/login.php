b80d363e09b632bc432bca8af3e480144e7bc05b
BTbeta <113897620+BTbeta@users.noreply.github.com>
2023-11-06 17:42:38 +0000

    update

---
+++ PHP|HTML/BES04/login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+	header('Location: riservata.php');
+	exit();
+}
+$errmsg = $_GET['errmsg'] ?? "";
+$user = $_POST["username"] ?? "";
+$password = $_POST["password"] ?? "";
+
+if(isset($_POST["submit"])) 
+{
+	if($user!="admin" || $password!="password")
+	{
+		$errmsg = "Credenziali sbagliate";
+	}
+	else
+	{
+		$_SESSION['user']=$user;
+		header("Location: riservata.php");
+		exit;
+	}
+}
+?>
+<html>
+<head>
+	<title>login di zeng</title>
+</head>
+<body>
+	<style>.err {color: #FF0000;}</style>
+	<p class="err"><?=$errmsg?></p>
+	<form name="frmlogin" method="post" action="<?=$_SERVER['PHP_SELF']?>">
+	Username:<input type="text" name="username"><br>
+	Password:<input type="password" name="password"><br>
+	<input type="submit" name="submit">
+	</form>
+	<a href="index.php">torna pagina principale</a>
+</body>
+</html>
---
+++ PHP|HTML/BES04/riservata.php
@@ -0,0 +1,18 @@
+<?php
+		session_start();
+		if (!isset($_SESSION['user'])) {
+			$url = 'login.php?errmsg=Fare prima il login&from=';
+			header("Location: $url");
+			exit();
+		}
+?>
+<html>
+	<head>
+		<title>benvenuto di zeng</title>
+	</head>
+	<body>
+		<h1>benvenuto admin<h1>
+		<a href="index.php">pagina principale</a><br><br>
+		<a href="logout.php">logout</a>
+	</body>
+</html>
