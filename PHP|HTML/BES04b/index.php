1fca3cefb16e86f94072645f8dd120be7a356b95
BTbeta <113897620+BTbeta@users.noreply.github.com>
2023-11-10 09:26:17 +0100

    Create index.php

---
+++ PHP|HTML/BES04b/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>sito di zeng</title>
+	</head>
+	<body>
+		<h1>benvenuto nel sito di zeng</h1>
+		<a href="riservata.php">area riservato</a><br><br>
+		<a href="login.php">login</a>
+	</body>
+</html>
